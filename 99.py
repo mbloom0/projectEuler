a2c8f098f017b9a24afcb35a81153e18f9cc39ee
mbloom0 <90012062+mbloom0@users.noreply.github.com>
2022-01-30 16:44:39 -0800

    completed problem 99

---
+++ 99.py
@@ -0,0 +1,35 @@
+import numpy as np
+import csv
+import math
+#Largest exponential
+  #Problem 99
+#Comparing two numbers written in index form like 211 and 37 is not difficult, as any calculator would confirm that 211 = 2048 < 37 = 2187.
+#However, confirming that 632382518061 > 519432525806 would be much more difficult, as both numbers contain over three million digits.
+#Using base_exp.txt (right click and 'Save Link/Target As...'), a 22K text file containing one thousand lines with a base/exponent pair on each line, determine which line number has the greatest numerical value.
+
+with open('base_exp.txt') as file_obj:
+    reader_obj=csv.reader(file_obj)
+    
+   
+    base_exp = [] 
+    for row in reader_obj:
+        base_exp.append(row)
+
+    print(base_exp)
+    
+    answers = [math.log(int(x[0]))* int(x[1]) for x in base_exp]
+
+    #indexVector = np.array(len(answers))
+    #for x in len(answers):
+    #    indexVector[x] = x
+
+    for x in range(len(answers)):
+        answers[x] = (answers[x],x)
+
+    #answers.concatenate(indexVector,axis=1)
+    answers.sort(key=lambda x: x[0])
+
+#
+#    np.sort(answers)
+    print(answers)
+    #Last answer's index +1 = line number = answer to problem
